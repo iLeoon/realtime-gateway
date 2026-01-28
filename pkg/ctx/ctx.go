b3810b9dddee6c087b9cbf05bb5752044d2abf02
iLeoon <ahmedeihab709@yahoo.com>
2026-01-28 19:27:22 +0200

    refactor: change getuserid to userid

--- pkg/ctx/ctx.go
+++ pkg/ctx/ctx.go
@@ -4,12 +4,12 @@ import "context"
 
 type ctxUserID struct{}
 
-func SetUserIDCtx(ctx context.Context, userID string) context.Context {
+func SetUserId(ctx context.Context, userID string) context.Context {
 	return context.WithValue(ctx, ctxUserID{}, userID)
 
 }
 
-func GetUserIDCtx(ctx context.Context) (string, bool) {
+func UserId(ctx context.Context) (string, bool) {
 	id, ok := ctx.Value(ctxUserID{}).(string)
 	return id, ok
 }
