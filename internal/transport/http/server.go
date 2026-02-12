42006def5eda826da62c7fdd445c592e5724afc0
iLeoon <ahmedeihab709@yahoo.com>
2026-02-12 20:00:35 +0200

    refactor(http): change folder name from helpers to resource

--- internal/transport/http/server.go
+++ internal/transport/http/server.go
@@ -5,12 +5,12 @@ import (
 
 	"github.com/go-playground/validator/v10"
 	"github.com/iLeoon/realtime-gateway/internal/config"
-	"github.com/iLeoon/realtime-gateway/internal/transport/http/helpers/auth"
-	"github.com/iLeoon/realtime-gateway/internal/transport/http/helpers/conversation"
-	"github.com/iLeoon/realtime-gateway/internal/transport/http/helpers/token"
-	"github.com/iLeoon/realtime-gateway/internal/transport/http/helpers/user"
 	"github.com/iLeoon/realtime-gateway/internal/transport/http/middleware"
-	"github.com/iLeoon/realtime-gateway/internal/transport/http/routes"
+	"github.com/iLeoon/realtime-gateway/internal/transport/http/resource/auth"
+	"github.com/iLeoon/realtime-gateway/internal/transport/http/resource/conversation"
+	"github.com/iLeoon/realtime-gateway/internal/transport/http/resource/token"
+	"github.com/iLeoon/realtime-gateway/internal/transport/http/resource/user"
+	"github.com/iLeoon/realtime-gateway/internal/transport/http/resource/websocket"
 	"github.com/iLeoon/realtime-gateway/internal/transport/http/services/validation"
 	"github.com/iLeoon/realtime-gateway/pkg/logger"
 	"github.com/jackc/pgx/v5/pgxpool"
@@ -22,49 +22,49 @@ type Handler interface {
 
 func Start(conf *config.Config, db *pgxpool.Pool, ws http.Handler) {
 	rootMux := http.NewServeMux()
-	apiMux := http.NewServeMux()
 
-	var handler http.Handler = apiMux
 	// Initializing the validator
 	validator := validator.New(validator.WithRequiredStructEnabled())
 	validation.Init(validator)
 
 	// Wraping the api mux with ValidateHeader.
-	handler = middleware.ValidateHeaders(handler)
+	middleware.ValidateHeaders(rootMux)
 
 	// Wraping the api mux with CORS.
-	handler = middleware.Cors(handler, conf)
+	middleware.Cors(rootMux, conf)
 
 	jwtService := token.NewService(conf)
 
 	authRepo := auth.NewRepo(db)
 	authService := auth.NewService(conf, authRepo)
+	authHandler := auth.NewHandler(authService, jwtService)
 
 	userRepo := user.NewRepo(db)
 	userServ := user.NewService(userRepo)
+	userHandler := user.NewHandler(userServ)
 
 	convRepo := conversation.NewRepo(db)
 	convServ := conversation.NewService(convRepo)
+	convHandler := conversation.NewHandler(convServ)
+
+	wsHandler := websocket.NewHandler(jwtService)
 
 	// Specifying the version.
-	rootMux.Handle("/api/v1.0/", http.StripPrefix("/api/v1.0", handler))
+	rootMux.Handle("/api/v1.0/", http.StripPrefix("/api/v1.0", rootMux))
 
-	// Defining handlers
-	authHandler := routes.Auth(authService, jwtService)
-	usersHandler := middleware.AuthGuard(routes.User(userServ), jwtService)
-	conversationHandler := middleware.AuthGuard(routes.Conversation(convServ), jwtService)
-	wsHandler := middleware.AuthGuard(routes.Websocket(jwtService), jwtService)
+	userMux := userHandler.RegisterRoutes()
+	authMux := authHandler.RegisterRoutes()
+	convMux := convHandler.RegisterRoutes()
+	wsMux := wsHandler.RegsiterRoutes()
 
-	apiMux.Handle("/auth/", authHandler)
-	apiMux.Handle("/users/", usersHandler)
+	rootMux.Handle("/auth/", authMux)
+	rootMux.Handle("/users/", middleware.AuthGuard(userMux, jwtService))
+	rootMux.Handle("/conversations/", middleware.AuthGuard(convMux, jwtService))
+	rootMux.Handle("/conversations", middleware.AuthGuard(convMux, jwtService))
+	rootMux.Handle("/ws/", middleware.AuthGuard(wsMux, jwtService))
 
-	apiMux.Handle("/conversations/", conversationHandler)
-	apiMux.Handle("/conversations", conversationHandler)
+	rootMux.Handle("/ws", middleware.ValidateWsTicket(ws, jwtService))
 
-	apiMux.Handle("/ws/", wsHandler)
-	apiMux.Handle("/ws", middleware.ValidateWsTicket(ws, jwtService))
-
-	logger.Info("The websocekt server is up and running..")
-	logger.Info("Listening to http requests")
+	logger.Info("The http server is up and running..")
 	http.ListenAndServe(conf.HttpPort, rootMux)
 }
